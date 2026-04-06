e527a30b35e1f0b29dbaa649a6be129ab224f22f
MINEDUCYT <MINEDUCYT@DESKTOP-CC30LNJ>
2026-04-06 14:23:30 -0600

    Agregar archivos de proyecto.

---
+++ WEOG20260406.AppSeguridadWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEOG20260406.AppSeguridadWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
