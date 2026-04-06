b1ca996a033a038550384f32e1dc0a5c34c6a7c0
williamernesto ojedagomez <williameperron@gmail.com>
2026-04-06 14:48:28 -0600

    William, cree clase AppDbContext

---
+++ WEOG20260406.AppSeguridadWeb/Models/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        string passwordHash = "$2a$11$hDEUK3aSHfrxt4SDvkV94.BHbSk2dfR8AX0/.puPHXqVDWcPAwyc6";  // Admin123
+        modelBuilder.Entity<Usuario>().HasData(
+            new Usuario
+            {
+                Id = 1,
+                Login = "admin",
+                PasswordHash = passwordHash,
+                Rol = "Administrador",
+                EstaActivo = true
+            }
+        );
+    }
+}
+
+
---
+++ WEOG20260406.AppSeguridadWeb/Models/Usuario.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("Usuarios")]
+public class Usuario
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "El nombre de usuario es obligatorio")]
+    [StringLength(50)]
+    public string Login { get; set; }
+
+    [Required(ErrorMessage = "La contraseña es obligatoria")]
+    [StringLength(100)]
+    [DataType(DataType.Password)]
+    [Display(Name = "Contraseña")]
+    public string PasswordHash { get; set; }
+
+    [NotMapped]
+    [Required(ErrorMessage = "Debe confirmar la contraseña")]
+    [Compare("PasswordHash", ErrorMessage = "Las contraseñas no coinciden")]
+    [StringLength(100)]
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirmar Contraseña")]
+    public string ConfirmarPassword { get; set; }
+
+    [Required]
+    [StringLength(20)]
+    [AllowedValues("Administrador", "Editor", "Usuario", ErrorMessage = "Seleccione un rol válido")]
+    public string Rol { get; set; }
+
+    public bool EstaActivo { get; set; } = true;
+}
+
+
+
+
+
