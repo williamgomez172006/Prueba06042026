cc4d079cc2d6b71ae9e976c711095a286062ebcb
William Ernesto Ojeda Gomez <williameperron@gmail.com>
2026-04-06 23:50:35 -0600

    william ajuste en progrm

--- WEOG20260406.AppSeguridadWeb/Program.cs
+++ WEOG20260406.AppSeguridadWeb/Program.cs
@@ -1,6 +1,6 @@
-
 using WEOG20260406.AppSeguridadWeb.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,27 +11,43 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SameSite = SameSiteMode.None;
+        options.ExpireTimeSpan = TimeSpan.FromHours(8);
+        options.Cookie.IsEssential = true;
+        options.SlidingExpiration = true;
+
+        // 🔥 CORREGIDO AQUÍ
+        options.LoginPath = "/Usuario/Login";
+        options.AccessDeniedPath = "/Usuarios/AccessDenied";
+        options.LogoutPath = "/Usuarios/Logout";
+    });
+
+// 🔥 AGREGADO
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
 
-app.UseAuthorization();
-
 app.MapStaticAssets();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
-
 app.Run();
-
