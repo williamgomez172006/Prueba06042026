c9fb07149a4f1ad9080855d0e77dff9d1d0e8084
William Ernesto Ojeda Gomez <williameperron@gmail.com>
2026-04-06 23:49:51 -0600

    william usuario creacion de login, acces, change

---
+++ WEOG20260406.AppSeguridadWeb/Controllers/UsuariosController.cs
@@ -0,0 +1,259 @@
+﻿using WEOG20260406.AppSeguridadWeb.Models;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+// Referencia para autenticación y autorización
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+namespace WEOG20260406.AppSeguridadWeb.Controllers
+{
+    [Authorize]
+    public class UsuariosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public UsuariosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Usuarios       
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Usuarios.ToListAsync());
+        }
+
+        // GET: Usuarios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuarios
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+        [Authorize(Roles = "Administrador,Usuario")]
+        // GET: Usuarios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Usuarios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrador,Usuario")]
+        public async Task<IActionResult> Create([Bind("Login,PasswordHash,ConfirmarPassword,Rol,EstaActivo")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                usuario.PasswordHash = BCrypt.Net.BCrypt.HashPassword(usuario.PasswordHash);
+                _context.Add(usuario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuario);
+        }
+        [Authorize(Roles = "Administrador,Editor")]
+        // GET: Usuarios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuarios.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrador,Editor")]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Login,Rol,EstaActivo")] Usuario usuario)
+        {
+            if (id != usuario.Id)
+            {
+                return NotFound();
+            }
+            try
+            {
+                var usuarioData = await _context.Usuarios.AsNoTracking().FirstOrDefaultAsync(u => u.Id == id);
+                if (usuarioData == null)
+                {
+                    return NotFound();
+                }
+                usuarioData.Login = usuario.Login;
+                usuarioData.Rol = usuario.Rol;
+                usuarioData.EstaActivo = usuario.EstaActivo;
+                _context.Update(usuarioData);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, $"Ocurrió un error : {ex.Message}");
+                return View(usuario);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Usuarios/Delete/5
+        [Authorize(Roles = "Administrador")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuarios
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrador")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+            if (usuario != null)
+            {
+                _context.Usuarios.Remove(usuario);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        [AllowAnonymous]
+        public IActionResult Login(string? returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            Usuario usuario = new Usuario();
+            return View(usuario);
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login([Bind("Login,PasswordHash")] Usuario usuario, string? returnUrl = null)
+        {
+            try
+            {
+                ViewData["ReturnUrl"] = returnUrl;
+                if (usuario == null || string.IsNullOrEmpty(usuario.Login) ||
+                    string.IsNullOrEmpty(usuario.PasswordHash))
+                {
+                    ModelState.AddModelError(string.Empty, "El login y la contraseña son requeridos.");
+                    return View(usuario);
+                }
+                var usuarioDB = await _context.Usuarios.FirstOrDefaultAsync(u => u.Login == usuario.Login &&
+                u.EstaActivo == true);
+                if (usuarioDB == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Nombre de usuario o contraseña incorrectos.");
+                    return View(usuario);
+                }
+                bool esValido = BCrypt.Net.BCrypt.Verify(usuario.PasswordHash, usuarioDB.PasswordHash);
+                if (esValido == false)
+                {
+                    ModelState.AddModelError(string.Empty, "Nombre de usuario o contraseña incorrectos.");
+                    return View(usuario);
+                }
+                var claims = new List<Claim>{
+                        new Claim(ClaimTypes.Name, usuarioDB.Login),
+                        new Claim(ClaimTypes.Role, usuarioDB.Rol),
+                        new Claim("Id", usuarioDB.Id.ToString())
+                    };
+                var claimsIdentity = new ClaimsIdentity(
+                        claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                await HttpContext.SignInAsync(
+                       CookieAuthenticationDefaults.AuthenticationScheme,
+                       new ClaimsPrincipal(claimsIdentity));
+                if (!string.IsNullOrWhiteSpace(returnUrl))
+                    return Redirect(returnUrl);
+                else
+                    return RedirectToAction("Index", "Home", new { area = "" });
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, $"Ocurrió un error al intentar iniciar sesión: {ex.Message}");
+                return View(usuario);
+            }
+
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Usuarios");
+        }
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+        public async Task<IActionResult> ChangePassword()
+        {
+            var userIdClaim = User.FindFirst("Id");
+            if (userIdClaim != null)
+            {
+                int userId = int.Parse(userIdClaim.Value);
+                var usuario = await _context.Usuarios.FindAsync(userId);
+                if (usuario == null)
+                    return NotFound();
+                return View(usuario);
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ChangePassword([Bind("Id,Login,PasswordHash,ConfirmarPassword")] Usuario usuario)
+        {
+            try
+            {
+                var usuarioData = await _context.Usuarios.AsNoTracking().FirstOrDefaultAsync(u => u.Id == usuario.Id);
+                if (usuarioData == null)
+                {
+                    return NotFound();
+                }
+                usuarioData.PasswordHash = BCrypt.Net.BCrypt.HashPassword(usuario.PasswordHash);
+                _context.Update(usuarioData);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, $"Ocurrió un error : {ex.Message}");
+                return View(usuario);
+            }
+            return RedirectToAction(nameof(Login));
+        }
+    }
+}
